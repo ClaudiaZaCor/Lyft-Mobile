23288ea0dfebde80eda212497cb7e5fd19794667
ClaudiaZaCor <claudia.zacor@gmail.com>
2017-12-29 03:35:10 -0500

    subiendo avance

---
+++ js/signup.js
@@ -0,0 +1,6 @@
+/*
+
+expresion regular para validar numero telefonico
+
+^\+?\d{1,3}?[- .]?\(?(?:\d{2,3})\)?[- .]?\d\d\d[- .]?\d\d\d\d$ */
+
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Reto Lyft
+
+
+## Desarrollado con:
+
+`HTML5` `Jquery` `CSS3` `Bootstrap`
+
+##  Realizado por:
+
+* [Claudia Zarate] 
